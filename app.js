7bb5ef76df18baf8c62a23ae42b3fe4182b057ec
Barnes <jbarnes13@una.edu>
2023-01-24 16:41:11 -0600

    set up stuff smile

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World! This is Billy the Cat!')
 })
 
+app.get('/show', (req, res) => {
+
+    res.sendFile('index.html' , { root : __dirname});
+
+})
+app.get('/ejs', (req, res) => {
+
+    //yes
+    res.render('index');
+
+})
 console.log('in the node console');
 
 app.listen(PORT, () => {
